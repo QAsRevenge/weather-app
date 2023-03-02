d5af31437b3faac4a331f23a1bedeba913743f41
QAsRevenge <hampus.e.molin@gmail.com>
2023-03-03 00:12:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=65e49f59506351175f9a6f2e7102c93f&lang=sv`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  function getTime(dt, timezone) {
+    const utc_seconds = parseInt(dt, 10) + parseInt(timezone, 10);
+    const utc_milliseconds = utc_seconds * 1000;
+    const local_date = new Date(utc_milliseconds).toUTCString().slice(17, 22);
+    return local_date;
+  }
+
+/* Clear, Rain, Haze, Snow, Clouds, */ 
+
+  return (
+    <div className="container">
+      <div className="search-box">
+        <i className="fa-solid fa-location-dot"></i>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+        <button className="fa-solid fa-magnifying-glass"></button>
+      </div>
+          <div className="location">
+        <p>{data.name} {data.sys ? <span>{data.sys.country}</span> : null}</p>
+        {data.main ? getTime(data.dt, data.timezone) : null}
+      </div>
+      <div className="weatherImage">
+         {data.weather ? 
+          <img src={"src/images/" + data.weather[0].main + ".gif"}></img>
+          : null
+        }
+      </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}<span>°C</span></h1> : null}
+      </div>
+        {data.weather ? <h1 className='desc'>{data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</h1> : null}
+
+
+      {/*{data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }*/}
+
+
+
+    </div>
+  );
+}
+
+export default App;
